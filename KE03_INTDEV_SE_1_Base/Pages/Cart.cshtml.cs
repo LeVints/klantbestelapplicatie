70f9d83a326f03bd78758e2baff4a68d6cedcb68
LebVi <lebvince@hotmail.com>
2025-05-21 18:59:12 +0200

    cart problemen

--- KE03_INTDEV_SE_1_Base/Pages/Cart.cshtml.cs
+++ KE03_INTDEV_SE_1_Base/Pages/Cart.cshtml.cs
@@ -1,34 +1,42 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
-public class CartModel : PageModel
+namespace KE03_INTDEV_SE_1_Base.Pages
 {
-    public class CartItem
+    public class CartModel : PageModel
     {
-        public int ProductId { get; set; }
-        public string ProductName { get; set; } = string.Empty;
-        public int Quantity { get; set; }
-        public decimal UnitPrice { get; set; }
-    }
-
-    [BindProperty]
-    public List<CartItem> Items { get; set; } = new List<CartItem>();
-
-    public void OnGet()
-    {
-        // TODO: haal echte winkelwagen-items op uit je DAL of sessie
-        Items = new List<CartItem>
+        // DTO voor een item in de winkelwagen
+        public class CartItem
         {
-            new CartItem { ProductId = 1, ProductName = "Product A", Quantity = 2, UnitPrice = 9.99m },
-            new CartItem { ProductId = 2, ProductName = "Product B", Quantity = 1, UnitPrice = 14.50m }
-        };
-    }
+            public int ProductId { get; set; }
+            public string ProductName { get; set; } = string.Empty;
+            public int Quantity { get; set; }
+            public decimal UnitPrice { get; set; }
+        }
 
-    public IActionResult OnPostRemove(int productId)
-    {
-        Items = Items.Where(i => i.ProductId != productId).ToList();
-        return Page();
+        // Zorg dat Items nooit null is
+        [BindProperty]
+        public List<CartItem> Items { get; set; } = new List<CartItem>();
+
+        public void OnGet()
+        {
+            // TODO: vervang deze voorbeelddata door echte data uit je DAL of uit de sessie
+            Items = new List<CartItem>
+            {
+                new CartItem { ProductId = 1, ProductName = "Product A", Quantity = 2, UnitPrice = 9.99m },
+                new CartItem { ProductId = 2, ProductName = "Product B", Quantity = 1, UnitPrice = 14.50m }
+            };
+        }
+
+        public IActionResult OnPostRemove(int productId)
+        {
+            // TODO: pas deze logica aan zodat je uit je DAL of sessie verwijdert
+            Items = Items.Where(i => i.ProductId != productId).ToList();
+
+            // Blijf op dezelfde pagina en toon het aangepaste overzicht
+            return Page();
+        }
     }
 }
