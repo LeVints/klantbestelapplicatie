c927f18787afd4ea3ecb2f7f0c361b09ee07aabf
LebVi <lebvince@hotmail.com>
2025-05-22 12:24:17 +0200

    Informatien wordt getoond in overzicht

--- KE03_INTDEV_SE_1_Base/Pages/Overzicht.cshtml.cs
+++ KE03_INTDEV_SE_1_Base/Pages/Overzicht.cshtml.cs
@@ -11,20 +11,29 @@ namespace KE03_INTDEV_SE_1_Base.Pages
     {
         private readonly ILogger<OverzichtModel> _logger;
         private readonly ICustomerRepository _customerRepository;
+        private readonly IOrderRepository _orderRepository;
 
         public IList<Customer> Customers { get; set; }
+        public IList<Order> Orders { get; set; }
 
-        public OverzichtModel(ILogger<OverzichtModel> logger, ICustomerRepository customerRepository)
+        public OverzichtModel(
+            ILogger<OverzichtModel> logger,
+            ICustomerRepository customerRepository,
+            IOrderRepository orderRepository)
         {
             _logger = logger;
             _customerRepository = customerRepository;
+            _orderRepository = orderRepository;
             Customers = new List<Customer>();
+            Orders = new List<Order>();
         }
 
         public void OnGet()
         {
             Customers = _customerRepository.GetAllCustomers().ToList();
+            Orders = _orderRepository.GetAllOrders().ToList();
             _logger.LogInformation($"Aantal klanten geladen: {Customers.Count}");
+            _logger.LogInformation($"Aantal orders geladen: {Orders.Count}");
         }
     }
 }
