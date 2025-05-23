74de84bb9722b3c6fa3f3a76a697f6768c216ed7
LebVi <lebvince@hotmail.com>
2025-05-23 12:31:17 +0200

    bestel knop

--- KE03_INTDEV_SE_1_Base/Pages/Cart.cshtml.cs
+++ KE03_INTDEV_SE_1_Base/Pages/Cart.cshtml.cs
@@ -1,13 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Http;
-using System.Text.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System;
 using KE03_INTDEV_SE_1_Base.Helpers;
+using DataAccessLayer.Interfaces;
+using DataAccessLayer.Models;
 
 namespace KE03_INTDEV_SE_1_Base.Pages
 {
     public class CartModel : PageModel
     {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IProductRepository _productRepository;
+
+        public CartModel(IOrderRepository orderRepository, IProductRepository productRepository)
+        {
+            _orderRepository = orderRepository;
+            _productRepository = productRepository;
+        }
+
         public class CartItem
         {
             public int ProductId { get; set; }
@@ -21,6 +34,42 @@ namespace KE03_INTDEV_SE_1_Base.Pages
         public void OnGet()
         {
             Items = HttpContext.Session.GetObject<List<CartItem>>("Cart") ?? new List<CartItem>();
+        }
+
+        public IActionResult OnPostPlaceOrder()
+        {
+            var items = HttpContext.Session.GetObject<List<CartItem>>("Cart");
+            if (items == null || !items.Any())
+            {
+                TempData["Message"] = "Winkelwagen is leeg!";
+                return RedirectToPage("Cart");
+            }
+
+            // Hardcoded klant-ID; in productie kies je de ingelogde gebruiker
+            var customerId = 1;
+
+            var newOrder = new Order
+            {
+                CustomerId = customerId,
+                OrderDate = DateTime.Now
+                // Products-lijst wordt hieronder gevuld
+            };
+
+            foreach (var item in items)
+            {
+                var product = _productRepository.GetProductById(item.ProductId);
+                if (product != null)
+                {
+                    newOrder.Products.Add(product); // ✅ lijst is al geïnitialiseerd in het model
+                }
+            }
+
+            _orderRepository.AddOrder(newOrder);
+
+            HttpContext.Session.Remove("Cart");
+
+            TempData["Message"] = "Bestelling succesvol geplaatst!";
+            return RedirectToPage("Cart");
         }
     }
 }
